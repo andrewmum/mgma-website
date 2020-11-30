1495c164881592bab76c5e9f327818ec2e435ca9
Andrew Mumford <36317914+andrewmum@users.noreply.github.com>
2020-11-30 14:11:37 -0500

    removed import in layout.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import React from "react"
 // import { Link } from "gatsby"
 // import scrollTo from 'gatsby-plugin-smoothscroll';
+import HeroSection from "../components/HeroSection"
+import Image from '../components/image'
+import Layout from "../components/layout"
+import Meeting from '../components/Meeting'
+import SEO from "../components/seo"
+import Purpose from '../components/Purpose'
+
 import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 
-
-import Image from '../components/image'
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import HeroSection from "../components/HeroSection"
-import Purpose from '../components/Purpose'
-import Meeting from '../components/Meeting'
 
 const IndexPage = () => (
   
